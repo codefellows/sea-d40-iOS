ae7ad97a3f90c59d77429a53b0294b2722424578
bradleypj@icloud.com <bradleypj@icloud.com>
2015-08-03 16:32:50 -0700

    added good twitter project

---
+++ Sample Code/Week1Twitter/Week1Twitter/Post.swift
@@ -0,0 +1,17 @@
+//
+//  Post.swift
+//  TwitterCF
+//
+//  Created by Bradley Johnson on 8/3/15.
+//  Copyright (c) 2015 BPJ. All rights reserved.
+//
+
+import Foundation
+
+
+struct Post {
+  let username : String
+  let firstname : String
+  let profilePicture : String
+}
+
